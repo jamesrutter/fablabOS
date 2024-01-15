8eceb20fcf454c939c20920a9efb32615eb6350e
James Rutter <jamesdavidrutter@gmail.com>
2024-01-15 08:13:06 -0500

    add flask api template
    The initial framework contains a basic structure for creating an API via Flask. It contains a database initialization cli tool and basic auth tools for registering and authenticating users

--- schedulr/schema.sql
+++ schedulr/schema.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS `user`;
-DROP TABLE IF EXISTS `post`;
+DROP TABLE IF EXISTS `project`;
+DROP TABLE IF EXISTS `equipment`;
+DROP TABLE IF EXISTS `reservation`;
+DROP TABLE IF EXISTS `time_slot`;
 CREATE TABLE `user` (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE NOT NULL,
@@ -7,9 +10,29 @@ CREATE TABLE `user` (
     role TEXT NOT NULL
 );
 CREATE TABLE `project` (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    description TEXT NOT NULL,
-    user_id INTEGER NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES user(id)
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `title` TEXT NOT NULL,
+    `description` TEXT NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES user(id)
+);
+CREATE TABLE `equipment`(
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `name` TEXT NOT NULL,
+    `description` TEXT NOT NULL,
+);
+CREATE TABLE `reservation`(
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `time_slot_id` INTEGER NOT NULL,
+    `user_id` INTEGER NOT NULL,
+    `equipment_id` INTEGER NOT NULL,
+    FOREIGN KEY (`time_slot_id`) REFERENCES time_slot(id),
+    FOREIGN KEY (`user_id`) REFERENCES user(id),
+    FOREIGN KEY (`equipment_id`) REFERENCES equipment(id)
+);
+CREATE TABLE `time_slot`(
+    `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+    `start_time` DATETIME NOT NULL,
+    `end_time` DATETIME NOT NULL,
+    UNIQUE (`start_time`, `end_time`)
 );
