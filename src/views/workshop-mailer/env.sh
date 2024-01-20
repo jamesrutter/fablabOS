efbcb73cf085eb2ae05880e09693f0471cdfc993
James Rutter <jamesdavidrutter@gmail.com>
2024-01-20 04:37:25 -0500

    refactor fablabOS project level directory
    major structural change to align with best practices, also included a pyproject.toml, and updates to README

---
+++ src/views/workshop-mailer/env.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Google Account Settings
+export APP_PASSWORD='cpzf hcqj zena vzzf'
+echo "App Password:" $APP_PASSWORD
+export ACCOUNT_EMAIL='fablab@haystack-mtn.org'
+echo "User:" $ACCOUNT_EMAIL
+# SMTP Server
+export SMTP_SERVER='smtp.gmail.com'
+echo "SMTP Server:" $SMTP_SERVER
+export PORT='587'
+echo "Port:" $PORT
