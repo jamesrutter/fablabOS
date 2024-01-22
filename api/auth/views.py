9773603157e75ad5b42586d484a28ab5b7a51d65
James Rutter <jamesdavidrutter@gmail.com>
2024-01-22 11:22:18 -0500

    refactor users into auth views

--- api/auth/views.py
+++ api/auth/views.py
@@ -1,9 +1,15 @@
 from flask import g, request, session, make_response, current_app
+from flask.typing import ResponseReturnValue
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.exceptions import BadRequestKeyError
+from sqlite3 import Row, Connection, DatabaseError
 from api.db import get_db
+from api.auth.decorators import login_required
 from . import auth
 
+################################
+### Authentication Endpoints ###
+################################
 
 @auth.post('/register')
 def register():
@@ -59,6 +65,18 @@ def login():
     return make_response({"message": "Successfully logged in."}, 200)
 
 
+@auth.post(rule='/logout')
+def logout():
+    if g.user is None:
+        current_app.logger.error(
+            f"User attempted to log out without logging in.")
+        return make_response({"error": "You must be logged in to log out."}, 401)
+    session.clear()
+    current_app.logger.info(
+        f"User logged out.")
+    return make_response({"message": "Successfully logged out."}, 200)
+
+
 @auth.before_app_request
 def load_logged_in_user():
     current_app.logger.info('NEW REQUEST')
@@ -80,14 +98,133 @@ def log_session_data(response):
     return response
 
 
-@auth.post(rule='/logout')
-def logout():
-    if g.user is None:
-        current_app.logger.error(
-            f"User attempted to log out without logging in.")
-        return make_response({"error": "You must be logged in to log out."}, 401)
-    session.clear()
-    current_app.logger.info(
-        f"User logged out.")
-    return make_response({"message": "Successfully logged out."}, 200)
+###########################
+### User CRUD Endpoints ###
+###########################
 
+@auth.get('/users')
+@login_required
+def get_users() -> ResponseReturnValue:
+    """
+    Retrieve a list of all users from the database.
+
+    This endpoint fetches all user records from the database and returns them 
+    as a list of JSON objects. Each object represents a user with all their details.
+
+    Returns:
+        Response: A JSON list of all users. Each user is a dictionary 
+        containing user details. If no users are found, an empty list is returned.
+    """
+    try:
+        db: Connection = get_db()
+
+        # Select all users from the database, returns as a list of SQlite Row objects.
+        user_rows: list[Row] = db.execute(
+            'SELECT * FROM user').fetchall()
+
+        # Convert the list of SQlite Row objects to a list of dictionaries, which can be serialized to JSON.
+        return {'status': 'success', 'data': [dict(row) for row in user_rows]}, 200
+    except DatabaseError as e:
+        return {'status': 'error', 'message': e.args[0]}, 500
+
+
+@auth.get('/users/<int:id>')
+@login_required
+def user_detail() -> ResponseReturnValue:
+    """
+    Retrieve a single user from the database.
+
+    This endpoint fetches a single user record from the database and returns it 
+    as a JSON object. The object represents a user with all their details.
+
+    Args:
+        id (int): The ID of the user to retrieve.
+
+    Returns:
+        Response: A JSON object representing the user. If no user is found, 
+        an error message with a 404 status code is returned.
+    """
+    try:
+        db: Connection = get_db()
+
+        # Select the user from the database, returns as a SQlite Row object.
+        user_row: Row = db.execute(
+            'SELECT * FROM user WHERE id = ?', (id,)
+        ).fetchone()
+
+        if user_row is None:
+            return {'status': 'error', 'message': 'User not found.'}, 404
+
+        # Convert the SQlite Row object to a dictionary, which can be serialized to JSON.
+        return {'status': 'success', 'data': dict(user_row)}, 200
+    except DatabaseError as e:
+        return {'status': 'error', 'message': e.args[0]}, 500
+
+
+@auth.post('/users')
+@login_required
+def update_user(id: int, **kwargs) -> ResponseReturnValue:
+    """
+    Update a single user in the database.
+
+    This endpoint updates a single user record in the database and returns it 
+    as a JSON object. The object represents a user with all their details.
+
+    Args:
+        id (int): The ID of the user to update.
+        username (str): The username of the user.
+        password (str): The password of the user.
+        role (str): The role of the user.
+
+    Returns:
+        Response: A JSON object representing the user. If no user is found, 
+        an error message with a 404 status code is returned.
+    """
+    try:
+        db: Connection = get_db()
+
+        # Select the user from the database, returns as a SQlite Row object.
+        user_row: Row = db.execute(
+            'SELECT * FROM user WHERE id = ?', (id,)
+        ).fetchone()
+
+        if user_row is None:
+            return {'status': 'error', 'message': 'User not found.'}, 404
+
+        # Convert the SQlite Row object to a dictionary, which can be serialized to JSON.
+        return {'status': 'success', 'data': dict(user_row)}, 200
+    except DatabaseError as e:
+        return {'status': 'error', 'message': e.args[0]}, 500
+
+
+@auth.delete('/users/<int:id>')
+@login_required
+def delete_user(id: int):
+    """
+    Delete a single user from the database.
+
+    This endpoint deletes a single user record from the database and returns it 
+    as a JSON object. The object represents a user with all their details.
+
+    Args:
+        id (int): The ID of the user to delete.
+
+    Returns:
+        Response: A JSON object representing the user. If no user is found, 
+        an error message with a 404 status code is returned.
+    """
+    try:
+        db: Connection = get_db()
+
+        # Select the user from the database, returns as a SQlite Row object.
+        user_row: Row = db.execute(
+            'SELECT * FROM user WHERE id = ?', (id,)
+        ).fetchone()
+
+        if user_row is None:
+            return {'status': 'error', 'message': 'User not found.'}, 404
+
+        # Convert the SQlite Row object to a dictionary, which can be serialized to JSON.
+        return {'status': 'success', 'data': dict(user_row)}, 200
+    except DatabaseError as e:
+        return {'status': 'error', 'message': e.args[0]}, 500
